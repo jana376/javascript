b468c8b1396764df76a31404e5bc415ebbcfcb6a
Jana <jana@slavnic.com>
2024-11-14 10:00:54 +0100

    notfallkonsole

---
+++ Notfallkonsole/script.js
@@ -0,0 +1,15 @@
+function hoverAn(e){
+    e.target.parentElement.classList.add("hover");
+}
+
+function hoverAb(e){
+    e.target.parentElement.classList.remove("hover");
+}
+
+var schalter = document.getElementsByTagName("a")
+for (var i = 0; i < schalter.length; i++) {
+schalter[i].addEventListener("mouseover", hoverAn)
+schalter[i].addEventListener("mouseout", hoverAb)
+} 
+
+
